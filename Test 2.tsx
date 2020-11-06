139169d14264cb97d4c78727d3eacaa207b89339
vpilsudski <54854520+vpilsudski@users.noreply.github.com>
2020-11-06 17:08:30 +0100

    Test set externe

--- Test 2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Test 2" tilewidth="32" tileheight="32" tilecount="720" columns="24">
- <image source="Interiors_free_black_48x48.png" width="768" height="960"/>
-</tileset>
--- Test.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Test" tilewidth="48" tileheight="48" tilecount="320" columns="16">
- <image source="Interiors_free_black_48x48.png" width="768" height="960"/>
-</tileset>
